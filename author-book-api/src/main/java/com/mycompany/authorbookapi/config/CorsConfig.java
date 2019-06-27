1db4fba29f64ed2aab32f7a4a0021edaa81abfe7
Ivan Franchin <ivan.franchin@takeaway.com>
2019-06-27 22:50:54 +0200

    simplify corsFilter bean configuration

--- author-book-api/src/main/java/com/mycompany/authorbookapi/config/CorsConfig.java
+++ author-book-api/src/main/java/com/mycompany/authorbookapi/config/CorsConfig.java
@@ -1,9 +1,7 @@
 package com.mycompany.authorbookapi.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -14,7 +12,7 @@ import java.util.Collections;
 public class CorsConfig {
 
     @Bean
-    FilterRegistrationBean<CorsFilter> corsFilter() {
+    CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
@@ -22,9 +20,7 @@ public class CorsConfig {
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
+        return new CorsFilter(source);
     }
 
 }
