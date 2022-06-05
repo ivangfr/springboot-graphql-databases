930d9c13f113a68e72b9b5f8d7501b5fa0af6de1
Ivan Franchin <ivan.franchin@takeaway.com>
2022-06-06 01:22:04 +0200

    set true to auto-index-creation mongo property and cleanup code in MongoConfig class

--- book-review-api/src/main/java/com/mycompany/bookreviewapi/config/MongoConfig.java
+++ book-review-api/src/main/java/com/mycompany/bookreviewapi/config/MongoConfig.java
@@ -1,29 +1,9 @@
 package com.mycompany.bookreviewapi.config;
 
-import com.mycompany.bookreviewapi.model.Book;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.index.IndexOperations;
-import org.springframework.data.mongodb.core.index.IndexResolver;
-import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
-@RequiredArgsConstructor
 @EnableMongoAuditing
 @Configuration
 public class MongoConfig {
-
-    private final MongoTemplate mongoTemplate;
-    private final MongoMappingContext mongoMappingContext;
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void initIndicesAfterStartup() {
-        IndexOperations indexOps = mongoTemplate.indexOps(Book.class);
-        IndexResolver resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
-        resolver.resolveIndexFor(Book.class).forEach(indexOps::ensureIndex);
-    }
 }
