f4e72751d12446b0cf1470764adbb3ae032e04b6
Ivan Franchin <ivan.franchin@takeaway.com>
2024-11-24 14:44:05 +0100

    Replace Jib with Paketo Buildpacks

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-./mvnw clean compile jib:dockerBuild --projects author-book-api
-./mvnw clean compile jib:dockerBuild --projects book-review-api
+DOCKER_IMAGE_PREFIX="ivanfranchin"
+APP_VERSION="1.0.0"
+
+AUTHOR_BOOK_API_APP_NAME="author-book-api"
+BOOK_REVIEW_API_APP_NAME="book-review-api"
+
+AUTHOR_BOOK_API_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${AUTHOR_BOOK_API_APP_NAME}:${APP_VERSION}"
+BOOK_REVIEW_API_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${BOOK_REVIEW_API_APP_NAME}:${APP_VERSION}"
+
+SKIP_TESTS="true"
+
+./mvnw clean spring-boot:build-image --projects "$AUTHOR_BOOK_API_APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$AUTHOR_BOOK_API_DOCKER_IMAGE_NAME"
+./mvnw clean spring-boot:build-image --projects "$BOOK_REVIEW_API_APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$BOOK_REVIEW_API_DOCKER_IMAGE_NAME"
--- start-apps.sh
+++ start-apps.sh
@@ -8,7 +8,7 @@ echo "Starting author-book-api ..."
 docker run -d --rm --name author-book-api -p 8080:8080 \
   -e MYSQL_HOST=mysql -e ZIPKIN_HOST=zipkin -e BOOK_REVIEW_API_HOST=book-review-api -e SPRING_DATASOURCE_USERNAME=authorbookuser -e SPRING_DATASOURCE_PASSWORD=authorbookpass \
   --network=springboot-graphql-databases_default \
-  --health-cmd="curl -f http://localhost:8080/actuator/health || exit 1" \
+  --health-cmd='[ -z "$(echo "" > /dev/tcp/localhost/8080)" ] || exit 1' \
   ivanfranchin/author-book-api:1.0.0
 
 wait_for_container_log "author-book-api" "Started"
@@ -19,7 +19,7 @@ echo "Starting book-review-api ..."
 docker run -d --rm --name book-review-api -p 9080:9080 \
   -e MONGODB_HOST=mongodb -e ZIPKIN_HOST=zipkin -e SPRING_DATA_MONGODB_USERNAME=bookreviewuser -e SPRING_DATA_MONGODB_PASSWORD=bookreviewpass \
   --network=springboot-graphql-databases_default \
-  --health-cmd="curl -f http://localhost:9080/actuator/health || exit 1" \
+  --health-cmd='[ -z "$(echo "" > /dev/tcp/localhost/9080)" ] || exit 1' \
   ivanfranchin/book-review-api:1.0.0
 
 wait_for_container_log "book-review-api" "Started"
