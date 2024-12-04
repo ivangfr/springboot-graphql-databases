463738056652b9fe33b62393167d3bbc128a6b4d
Ivan Franchin <ivan.franchin@takeaway.com>
2024-12-04 14:28:22 -0300

    Upgrade to Zipkin 3.4.1 and small code adjustment

--- book-review-api/src/main/java/com/ivanfranchin/bookreviewapi/graphql/BookReviewController.java
+++ book-review-api/src/main/java/com/ivanfranchin/bookreviewapi/graphql/BookReviewController.java
@@ -3,10 +3,10 @@ package com.ivanfranchin.bookreviewapi.graphql;
 import com.ivanfranchin.bookreviewapi.graphql.input.BookInput;
 import com.ivanfranchin.bookreviewapi.graphql.input.ReviewInput;
 import com.ivanfranchin.bookreviewapi.mapper.BookMapper;
-import com.ivanfranchin.bookreviewapi.service.BookService;
 import com.ivanfranchin.bookreviewapi.mapper.ReviewMapper;
 import com.ivanfranchin.bookreviewapi.model.Book;
 import com.ivanfranchin.bookreviewapi.model.Review;
+import com.ivanfranchin.bookreviewapi.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
@@ -62,7 +62,7 @@ public class BookReviewController {
     public Book addBookReview(@Argument String bookId, @Argument ReviewInput reviewInput) {
         Book book = bookService.validateAndGetBookById(bookId);
         Review review = reviewMapper.toReview(reviewInput);
-        book.getReviews().add(0, review);
+        book.getReviews().addFirst(review);
         return bookService.saveBook(book);
     }
 }
